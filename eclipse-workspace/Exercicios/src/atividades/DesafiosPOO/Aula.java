aa0dfe58a087b2260c762c24da1dfb549577be3c
Enzo Oliveira Finotti <3488358@ifgoiano.local>
2025-08-19 08:49:40 -0300

    Continuação do estudo de POO 08:49 19/08/2025

--- eclipse-workspace/Exercicios/src/atividades/DesafiosPOO/Aula.java
+++ eclipse-workspace/Exercicios/src/atividades/DesafiosPOO/Aula.java
@@ -8,8 +8,12 @@ public class Aula {
 		novaData.ImprimirData();
 		
 		Mercado cachorro_quente = new Mercado("Cachorro Quente", 10.50f);
+		Mercado pao_de_queijo = new Mercado("Pão de Queijo", 3.5f);
 		
-		cachorro_quente.getValorComDesconto(50);
+		Mercado.porcentagem = 60;
+		
+		cachorro_quente.getValorComDesconto();
+		pao_de_queijo.getValorComDesconto();
 		
 	}
 }
--- eclipse-workspace/Exercicios/src/atividades/DesafiosPOO/Mercado.java
+++ eclipse-workspace/Exercicios/src/atividades/DesafiosPOO/Mercado.java
@@ -5,21 +5,19 @@ public class Mercado {
 	String nome;
 	float preco;
 	
+	public static float porcentagem;
+	
 	public Mercado(String nome, float preco) {
 		this.nome = nome;
 		this.preco = preco;
-	}
-	
-	public static float aumentoDesconto(float valorInteiro) {
-		return (valorInteiro/100);
 	}
 	
 	public void getMercado() {
 		System.out.printf("Nome: %s -> Preço: %.2f \n", nome, preco);
 	}
 	
-	public void getValorComDesconto(float porcentagemValor) {
-		System.out.printf("O valor com esse desconto é: %.2f", preco * aumentoDesconto(porcentagemValor)); 
+	public void getValorComDesconto() {
+		System.out.printf("O valor com esse desconto é: %.2f \n", preco - (preco * (porcentagem/100))); 
 	}
 	
 }
